2f07da0131004cc6b557b06f251eed1dac39687c
fanjianrong <ffffjr@163.com>
2017-06-07 15:09:10 +0800

    Create

---
+++ JRPlayer/JRPlayer/JRFFMpeg/JRFFVideoDecoder.h
@@ -0,0 +1,13 @@
+//
+//  JRFFVideoDecoder.h
+//  JRPlayer
+//
+//  Created by fanjianrong on 2017/6/7.
+//  Copyright © 2017年 樊健荣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRFFVideoDecoder : NSObject
+
+@end
---
+++ JRPlayer/JRPlayer/JRFFMpeg/JRFFVideoDecoder.m
@@ -0,0 +1,13 @@
+//
+//  JRFFVideoDecoder.m
+//  JRPlayer
+//
+//  Created by fanjianrong on 2017/6/7.
+//  Copyright © 2017年 樊健荣. All rights reserved.
+//
+
+#import "JRFFVideoDecoder.h"
+
+@implementation JRFFVideoDecoder
+
+@end

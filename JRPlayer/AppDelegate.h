f28c4d5578e0987d85dd48bb4a7ea563b5ee48c3
fanjianrong <ffffjr@163.com>
2017-06-07 14:51:36 +0800

    Create project

---
+++ JRPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRPlayer
+//
+//  Created by fanjianrong on 2017/6/7.
+//  Copyright © 2017年 樊健荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
